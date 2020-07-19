aa000be24928c5f51bf722bad63d209af574ef55
Md afratul Kaoser Taohid <taohid32@gmail.com>
2020-07-20 00:20:01 +0600

    Delete api_manager.dart

--- lib/api_manager.dart
+++
@@ -1,7 +0,0 @@
-library api_manager;
-
-/// Created by Taohid on 01, March, 2020
-/// Email: taohid32@gmail.com
-
-export 'api_manager_export.dart'
-    if (dart.library.io) 'api_manager_export.io.dart';
