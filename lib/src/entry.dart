d0b407d5be6d5563650b63797a3d4a1182b0860c
Md afratul Kaoser Taohid <taohid32@gmail.com>
2020-07-20 00:20:30 +0600

    Delete entry.dart

--- lib/src/entry.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dio/dio.dart';
-
-import 'api_manager.dart';
-
-/// Created by Taohid on 01, March, 2020
-/// Email: taohid32@gmail.com
-
-ApiManager createApiManager(BaseOptions _baseOptions) =>
-    throw UnsupportedError('');
